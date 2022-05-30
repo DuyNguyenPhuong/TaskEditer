44d8c03a6420d4e2a07acbdb55283323e04fc420
Sotatek-DuyNguyen <duy.nguyen4@sotatek.com>
2022-05-29 21:08:21 -0500

    29 May

--- src/orm.config.ts
+++ src/orm.config.ts
@@ -5,7 +5,7 @@ export const typeOrmConfig : TypeOrmModuleOptions = {
     "host": "localhost",
     "port": 5432,
     "username": "postgres",
-    "password": "root",
+    "password": "1234",
     "database": "test1",
     "entities": ["dist/**/*.entity{.ts,.js}"],
     "synchronize": true
